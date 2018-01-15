1cdd19a33921cebd8f1280571f9114ec622b2247
Deepanshi Bansal <deepanshibansal@ymail.com>
2018-01-15 12:53:56 +0530

    addition without using any arithmatic operator
    addition without arithmetic operator

---
+++ additionwithoutplus.java
@@ -0,0 +1,15 @@
+/*
+Created by Deepanshi Bansal
+*/	
+public class additionwithoutplus {
+
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+while(b--!=0)
+a++;
+System.out.println("the addition of given 2 numbers is "+a);
+	}
+
+}
